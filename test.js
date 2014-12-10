168076ec48f345850dac0be605d44fb48e10ba23
jonschlinkert <dev@sellside.com>
2014-12-09 19:25:34 -0500

    update tests

--- test.js
+++ test.js
@@ -11,11 +11,11 @@ var regex = require('./index');
 
 describe('utc regex', function() {
   it('should match utc input', function() {
-    assert.equal(regex.test('Sun, 07 Dec 2014 09:44:50 GMT'), true);
+    assert.equal(regex().test('Sun, 07 Dec 2014 09:44:50 GMT'), true);
   });
 
   it('should catch incorrect input', function() {
-    assert.equal(regex.test('Sun, 07 De 2014 09:44:50 GMT'), false);
-    assert.equal(regex.test('Sun 07 De 2014 09:44:50 GMT'), false);
+    assert.equal(regex().test('Sun, 07 De 2014 09:44:50 GMT'), false);
+    assert.equal(regex().test('Sun 07 De 2014 09:44:50 GMT'), false);
   });
 });
